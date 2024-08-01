47c14306e061455e663254c114d2337bdc320105
ljmvictor <99060896+ljmvictor@users.noreply.github.com>
2024-08-01 12:01:01 +0800

    edx-custom-icons.js

--- edx-custom-icons.js
+++ edx-custom-icons.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select all iframe tool elements with icons
-    var iconElements = document.querySelectorAll("iframe .icon-book");
+    // Include FontAwesome CSS
+    var fontAwesomeLink = document.createElement("link");
+    fontAwesomeLink.rel = "stylesheet";
+    fontAwesomeLink.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
+    document.head.appendChild(fontAwesomeLink);
 
-    // Change the class to video icon for each element
-    iconElements.forEach(function(icon) {
-        icon.classList.remove("icon-book");
-        icon.classList.add("icon-video"); // Ensure you have appropriate styling for .icon-video in your CSS
-    });
+    // Function to change the book icon to video icon
+    function changeIcons() {
+        // Select all iframe tool elements with book icons
+        var iconElements = document.querySelectorAll(".icon-book");
+
+        // Change the class to video icon for each element
+        iconElements.forEach(function(icon) {
+            icon.classList.remove("icon-book");
+            icon.classList.add("fa", "fa-video"); // FontAwesome video icon class
+        });
+    }
+
+    // Wait for FontAwesome to load before changing icons
+    fontAwesomeLink.onload = function() {
+        changeIcons();
+    };
 });
